ca4e2e4c5d7210e7c3872494829791aabb05d4ce
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2016-07-08 15:31:37 -0300

    Syncronizing local files and the public git repository

--- benchmarks.r
+++ benchmarks.r
@@ -208,3 +208,67 @@ benchmarkAll <- function(data,indexfield,valuefield,nps,crossvalidation_ratio){
 }
 
 
+
+benchmarkAll_Simple <- function(data,indexfield,valuefield,nps,crossvalidation_ratio){
+    builders <- list(Song = FitSongFTS, Chen = FitChenFTS, Yu = FitYuFTS, PFTS=FitPFTS, PWFTS=FitPWFTS)
+    
+    intervals <- c(FALSE, FALSE, FALSE, TRUE, TRUE)
+    
+    benchmarks <- list()
+    for(i in 1:length(builders)){
+		if(intervals[i]){
+			benchmarks[[i]] <- benchmarkInterval(builders[[i]], 
+				as.vector(data[,valuefield]),nps[i],trimf,NULL, crossvalidation_ratio)
+		} else {
+			benchmarks[[i]] <- benchmark(builders[[i]], 
+				as.vector(data[,valuefield]),nps[i],trimf,NULL, crossvalidation_ratio)
+		}
+	}
+    td <- length(data[,indexfield])
+    vh <- mean(data[,valuefield])
+    vi <- benchmarks[[1]]$validationIndex
+    
+    options(repr.plot.width=10, repr.plot.height=8)
+    layout(matrix(c(1,2,2), 3, 1, byrow = TRUE))
+        
+    plot(data[,indexfield],data[,valuefield],
+         type="l",main="Original data and cross validation sets",
+        xlab="t",ylab="F(t)")
+    text(min(data[,indexfield]),vh,"Training set",pos=4)
+    text(data[vi,indexfield],vh,"Validation set",pos=4)
+    abline(h=vh,v=data[vi,indexfield])
+    
+    test <- benchmarks[[1]]$validation
+        
+    miny <- min(test,na.rm=TRUE)
+    maxy <- max(test,na.rm=TRUE)    
+    for(i in 1:length(builders)) {
+		if(intervals[i]){
+			miny <- min(miny, min(benchmarks[[i]]$predictedLower,na.rm=TRUE))
+			maxy <- max(maxy, max(benchmarks[[i]]$predictedUpper,na.rm=TRUE))
+		} else { 
+			miny <- min(miny, min(benchmarks[[i]]$predicted,na.rm=TRUE))
+			maxy <- max(maxy, max(benchmarks[[i]]$predicted,na.rm=TRUE))
+		}
+    }
+        
+    plot(test, type="l", col=1, 
+         main="Model performance on validation set",xlab="t",ylab="F(t)",
+         ylim=c(miny*0.9,maxy*1.1))
+    lgd <- c("Original")
+    clrs <- c(1)
+    for(i in 1:length(builders)) {
+        lgd[i+1] <- benchmarks[[i]]$model$name 
+        clrs[i+1] <- i*7
+        if(intervals[i]){
+			lines(benchmarks[[i]]$predictedLower,type="l",col=i*7)
+			lines(benchmarks[[i]]$predictedUpper,type="l",col=i*7)
+		} else {
+			lines(benchmarks[[i]]$predicted,type="l",col=i*7)
+		}
+    }
+    legend("topright",legend=lgd, fill=clrs)
+    
+}
+
+
