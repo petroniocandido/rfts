d5a619312b075290fcd846128efdd8a920889711
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2016-08-09 15:33:54 -0300

    High Order Fuzzy Time Series

--- hofts.r
+++ hofts.r
@@ -1,3 +1,4 @@
+
 HOFLRG <- function(plhs, prhs){
     nc <- list(
         lhs = plhs,
@@ -21,13 +22,107 @@ HOFLRG <- function(plhs, prhs){
     } 
             
     nc$dump <- function() {
-		tmpl <- lhs[1]
-		if(length(lhs) > 1) for(i in 2:length(lhs)) tmpl <- paste(tmpl,lhs[i],sep=", ")
-        prhs <- sort(nc$rhs)
-        tmp <- paste(tmpl,prhs[1], sep=" -> ");
-        if(length(prhs) > 1) for(i in 2:length(prhs)) tmp <- paste(tmp,prhs[i],sep=", ")
+		tmpl <- toString(nc$lhs)
+		tmpr <- toString(sort(nc$rhs)) 
+        tmp <- paste(tmpl,tmpr, sep=" -> ");
         return (tmp)
     }
             
     return (nc);
+}
+
+HOFTS <- function(fsets,flrgs,l){
+    nc <- list(
+        name = "HOFTS",
+        fuzzySets = fsets,
+        flrg = flrgs,
+        npart = length(fsets),
+        lags = l
+    );
+    
+    nc$dump <- function() {
+        tmp <- ""
+        for(k in nc$flrg){
+            if(is.null(k)) k <- HOFLRG(fs$name, c(fs$name))
+            tmp <- sprintf("%s \n %s",tmp,k$dump());
+        }
+        return (tmp)
+    }
+    
+    nc$getMidpoints <- function(nflrg){
+        k <- nc$flrg[[ nflrg ]];
+        mp <- c()
+        
+        if(length(k$rhs) == 0) {
+			print("inexistente")
+            return (c( nc$fuzzySets[[ nflrg ]]$midpoint))
+        }
+        
+        for(i in 1:length(k$rhs)) mp[i] <- nc$fuzzySets[[ k$rhs[i] ]]$midpoint;
+        return (mp);
+    }
+    
+    nc$forecast <- function(x){
+		l <- length(x)
+		
+		if(l < nc$lags){
+			print("Dados insuficientes!")
+		}
+
+		ret <- c()
+
+		for(k in nc$lags:l) {
+			
+			prev <- fuzzySeries( x[ (k - nc$lags):k ],  nc$fuzzySets)
+			
+			lhs <- toString(prev)
+			
+			mp <- nc$getMidpoints( lhs )
+			
+			ret[k] <- (sum(mp)/length(mp))
+			
+		}	
+        return ( ret )       
+        
+    }
+    
+    return (nc)
+}
+
+
+FitHOFTS <- function(pdata,np,mf,parameters) {
+    nc <- list(
+        data = pdata,
+        npart = np,
+        membershipFunc = mf,
+        fuzzySets = UniversePartitioner(pdata,np,mf,"A"),
+        lags = parameters
+    );
+    
+    nc$genFLRG <- function(data){
+        flrgs <- list()
+        l <- length(data)
+        for(k in nc$lags:l){
+			
+			prev <- fuzzySeries( data[ (k - nc$lags):k ],  nc$fuzzySets)
+			
+			lhs <- toString(prev)
+			
+            if(is.null(flrgs[[lhs]])){
+                flrgs[[lhs]] <- HOFLRG(prev,c(data[k]));
+            } else {
+                flrgs[[lhs]] <- flrgs[[lhs]]$putRHS(data[k]);
+            }
+        }
+        return (flrgs)
+    } 
+    
+    nc$train <- function() {
+       fzydata <- fuzzySeries(nc$data,nc$fuzzySets);
+        flrgs <- nc$genFLRG(fzydata);
+        tmp <- HOFTS(nc$fuzzySets, flrgs, nc$lags);
+        return (tmp);
+    }
+    
+    return (nc)
 }
